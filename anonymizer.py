a8ccb7561d0a4c81979ac6db2ffa7557c0e73bf9
qiyuangong <qiyuangong@gmail.com>
2014-07-06 21:59:18 +0800

    Add basic fuctions

---
+++ anonymizer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#coding=utf-8
+from mondrian import mondrian
+from utils.read_data import read_data, read_tree
+from utils.evaluation import average_relative_error
+from utils.save_result import save_to_file
+import sys
+# Poulis set k=25, m=2 as default!
+
+if __name__ == '__main__':
+    K = 10
+    try:
+        K = int(sys.argv[1])
+    except:
+        pass
+    #read gentree tax
+    att_tree = read_tree()
+    #read record
+    trans = read_data()
+    # remove duplicate items
+    for i in range(len(trans)):
+        trans[i] = list(set(trans[i]))
+    print "Begin Partition"
+    result = partition(att_tree, trans, K)
+    # save_to_file(result)
+    print "Finish Partition!!"
+    # print "Begin Evaluation"
+    # are = average_relative_error(att_tree, trans, result)
+    # print "Average Relative Error: %.2f" % are
---
+++ mondrian.py
@@ -0,0 +1,21 @@
+
+def choose_dimension():
+
+
+
+def frequency_set(partiton, dim):
+
+
+def find_median(fs):
+
+
+
+def mondrian():
+
+    dim = choose_dimension()
+    fs = frequency_set(partiton, dim)
+    splitVal = find_median(fs)
+    lhs = 0 <= 
+    rhs = 0 >
+
+    return mondrian(rhs) U mondrian(lhs)
